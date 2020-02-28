76588f267f54a0a461674a4a01f2dcd46869889f
Unicorn-Meng <1079128259@qq.com>
2020-02-28 10:43:01 +0800

    This is the first C program!

--- One.c
+++ One.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+void First_C_Program(void);
+
 int main(void){
+  First_C_Program();
+  return 0;
+}
+
+void First_C_Program(void){
   int a,b;
   a=10;
   b=7;
   a=a*b;
   printf("%d%s",a,"\n");
-  printf("%s","This is first C program!");
-  return 0;
+  printf("%s","This is the first C program!");
 }
